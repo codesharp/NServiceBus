277b19c8f49e1c557992b42f22ac7ccac2ff55e5
John Simons <john.simons@nservicebus.com>
2012-09-29 11:17:28 +1000

    Spike on how to write a powershell cmdlet to install infrastructure.

---
+++ src/Powershell/InstallInfrastructureCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.PowershellTools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Reflection;
+    using System.Security.Principal;
+    using Installation;
+    using Installation.Environments;
+
+    [Cmdlet(VerbsLifecycle.Install, "Infrastructure")]
+    public class InstallInfrastructureCommand : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            GetInstallers<Windows>(typeof(INeedToInstallInfrastructure<>))
+                .ToList()
+                .ForEach(t => ((INeedToInstallInfrastructure)Activator.CreateInstance(t)).Install(WindowsIdentity.GetCurrent()));
+        }
+
+        private static IEnumerable<Type> GetInstallers<TEnvtype>(Type openGenericInstallType) where TEnvtype : IEnvironment
+        {
+            var listOfCompatibleTypes = new List<Type>();
+
+            var envType = typeof(TEnvtype);
+            while (envType != typeof(object))
+            {
+                listOfCompatibleTypes.Add(openGenericInstallType.MakeGenericType(envType));
+                envType = envType.BaseType;
+            }
+
+            foreach (var t in Assembly.GetExecutingAssembly().GetTypes())
+                foreach (var i in listOfCompatibleTypes)
+                    if (i.IsAssignableFrom(t))
+                        yield return t;
+        }
+    }
+}
